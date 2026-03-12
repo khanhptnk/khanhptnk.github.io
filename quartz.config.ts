748e8bbf6d7624312d3b52438e493568fb957be9
Khanh Nguyen (from Dev Box) <nguyenkhanh@microsoft.com>
2026-03-12 11:05:46 -0700

    Quartz sync: Mar 12, 2026, 11:05 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Ubuntu Mono",
-        body: "Ubuntu Mono",
+        header: "Inter",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
